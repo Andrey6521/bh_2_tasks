5cafe09a07414c024383c7ddacd916fcdb2c041d
Vyacheslav <rineisky@gmail.com>
2021-02-22 01:39:50 +0300

    исправил тест triangle.py

--- tests/variables/test_triangle.py
+++ tests/variables/test_triangle.py
@@ -6,9 +6,9 @@ from tasks.variables.triangle import triangle
 
 @pytest.mark.parametrize(
     "side_1, side_2, expected", [
-        (3, 4, (5, 14, 6)),
-        (5, 12, (13, 34, 30)),
-        (33, 56, (65, 178, 924)),
+        (3, 4, (5, 12, 6)),
+        (5, 12, (13, 30, 30)),
+        (33, 56, (65, 154, 924)),
      ]
 )
 def test_tip(side_1, side_2, expected):
